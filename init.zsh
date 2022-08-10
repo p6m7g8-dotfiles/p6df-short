8ed7f5638f27c3317b08c2e4cf61de5e12444843
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 16:05:46 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -7,7 +8,6 @@
 ######################################################################
 p6df::modules::short::deps() {
   ModuleDeps=(
-    p6m7g8-dotfiles/p6common
     p6m7g8-dotfiles/p6df-js
   )
 }
@@ -22,6 +22,8 @@ p6df::modules::short::deps() {
 p6df::modules::short::langs() {
 
   p6_js_npm_global_install "@shortcut-cli/shortcut-cli"
+
+  p6_return_void
 }
 
 ######################################################################
@@ -34,6 +36,8 @@ p6df::modules::short::langs() {
 p6df::modules::short::init() {
 
   p6df::modules::short::prompt::init
+
+  p6_return_void
 }
 
 ######################################################################
@@ -45,7 +49,7 @@ p6df::modules::short::init() {
 ######################################################################
 p6df::modules::short::prompt::init() {
 
-#  p6df::core::prompt::line::add "p6df::modules::short::prompt::line"
+  p6df::core::prompt::line::add "p6df::modules::short::prompt::line"
 }
 
 ######################################################################
@@ -56,16 +60,25 @@ p6df::modules::short::prompt::init() {
 #>
 ######################################################################
 p6df::modules::short::prompt::line() {
+
   p6_short_prompt_info
 }
 
 ######################################################################
 #<
 #
-# Function: p6_short_prompt_info()
+# Function: str str = p6_short_prompt_info()
+#
+#  Returns:
+#	str - str
 #
 #>
 ######################################################################
 p6_short_prompt_info() {
-  true
+
+  local str
+
+  str="short:\t\t  "
+
+  p6_return_str "$str"
 }
